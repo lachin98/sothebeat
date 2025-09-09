60755d370d4e7544bb690e55a77010eed3e7d751
lachin98 <lachin.amanov@jumpkz.com>
2025-09-09 17:57:02 +0500

    Fix index.html structure for Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+    outDir: "build",
   },
   server: {
     port: 3000,
-    host: true
-  }
-})
+    host: true,
+  },
+});
