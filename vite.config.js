8f3fd1cbefb1b60e61711241f3dd8bcc4383fe53
lachin98 <lachin.amanov@jumpkz.com>
2025-09-09 18:00:02 +0500

    Fix vite build - move index.html to root

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".",
   build: {
-    outDir: "build",
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
   },
   server: {
     port: 3000,
