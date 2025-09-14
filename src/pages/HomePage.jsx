ab006081d72b854af18d6c0cc5ef9ada66d838fc
lachin98 <l4ch4wk@yandex.kz>
2025-09-15 00:53:50 +0500

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -276,7 +276,7 @@ const HomePage = ({ user }) => {
               </div>
               <div className="user-points">
                 <div className="points-label">
-                  Баллыsss{" "}
+                  Баллы{" "}
                   {isUpdating && <span className="updating-indicator">🔄</span>}
                 </div>
                 <div className={`points-value ${isUpdating ? "updating" : ""}`}>
